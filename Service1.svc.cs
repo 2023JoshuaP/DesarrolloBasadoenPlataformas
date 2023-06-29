992604b820f9ea864f77421f1d50410eb2477c2e
2023JoshuaP <131817672+2023JoshuaP@users.noreply.github.com>
2023-06-29 11:14:04 -0500

    Add files via upload

---
+++ Service1.svc.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace PruebaLaboratorio
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de clase "Service1" en el código, en svc y en el archivo de configuración a la vez.
+    // NOTA: para iniciar el Cliente de prueba WCF para probar este servicio, seleccione Service1.svc o Service1.svc.cs en el Explorador de soluciones e inicie la depuración.
+    public class Service1 : IService1
+    {
+        public IList<string> getCiudades()
+        {
+            List<string> ciudades = new List<string>();
+
+            string basePath = AppDomain.CurrentDomain.BaseDirectory;
+            string filePath = Path.Combine(basePath, "TextFile1.txt");
+
+            try
+            {
+                string[] lines = File.ReadAllLines(filePath);
+                ciudades.AddRange(lines);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error al leer el archivo: " + ex.Message);
+            }
+
+            return ciudades;
+        }
+    }
+}
--- WebForm1.aspx.cs
+++ WebForm1.aspx.cs
@@ -6,6 +6,8 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using System.IO;
+using Desarrollo_Basado_en_Plataformas.ServiceReference1;
+using Desarrollo_Basado_en_Plataformas.ServiceReference2;
 
 namespace Registro
 {
@@ -15,14 +17,20 @@ namespace Registro
         {
             if (!IsPostBack)
             {
-                string filePath = Server.MapPath("~/App_Data/Ciudad.txt");
-                string[] ciudades = File.ReadAllLines(filePath);
+                //string filePath = Server.MapPath("~/App_Data/Lista.txt");
+                string[] ciudades = serviceCall();//File.ReadAllLines(filePath);
 
                 foreach (string ciudad in ciudades)
                 {
                     DropDownListCiudad.Items.Add(new ListItem(ciudad, ciudad));
                 }
             }
+        }
+        private String[] serviceCall()
+        {
+            Service1Client client = new Service1Client();
+            String[] ciudades = client.getCiudades();
+            return ciudades;
         }
         protected void ButtonEnviar_Click(object sender, EventArgs e)
         {
@@ -34,6 +42,9 @@ namespace Registro
             string ciudad = DropDownListCiudad.SelectedValue;
             string requerimientos = TextRequerimientos.Text;
 
+            Service2Client client = new Service2Client();
+            client.RegistroDatos(nombres, apellidos, sexo, correo, direccion, ciudad, requerimientos);
+
             StringBuilder datosRegistrados = new StringBuilder();
             datosRegistrados.AppendLine($"Nombres: {nombres}");
             datosRegistrados.AppendLine($"Apellidos: {apellidos}");
@@ -44,7 +55,7 @@ namespace Registro
             datosRegistrados.AppendLine($"Requerimientos: {requerimientos}");
 
             TextBoxDatosRegistrados.Text = datosRegistrados.ToString();
-            
+
             Text1.Text = string.Empty;
             Text2.Text = string.Empty;
             RadioButtonM.Checked = false;
